8e0bb1d9ad0ab56786f38dd0fc74da65798523d3
lauranurrea25-arch <laura.n.urrea.25@gmail.com>
2026-09-21 21:43:21 -0500

    Create tables for Nacionalidad, Actor, Reparto, and Pelicula
    Added tables for Nacionalidad, Actor, Reparto, and Pelicula with foreign key constraints.

--- glob-gusters-videoclub/sql/glob_gusters.sql
+++ glob-gusters-videoclub/sql/glob_gusters.sql
@@ -8,3 +8,52 @@ USE `glob_gusters`;
 
 -- Eliminación de la base de datos
 -- DROP DATABASE IF EXISTS `glob_gusters`;
+CREATE TABLE Nacionalidad(
+  NacionalidadID integer PRIMARY KEY,
+  Nombre varchar
+);
+
+CREATE TABLE Actor (
+  ActorID integer PRIMARY KEY,
+  Nombre varchar(255),
+  Sexo char (1),
+  NacionalidadID integer,
+    CONSTRAINT fk_Nacionalidad
+    FOREIGN KEY (NacionalidadID)
+    REFERENCES Nacionalidad (NacionalidadID)
+  
+);
+CREATE TABLE Reparto (
+  ActorID integer,
+  CONSTRAINT fk_Actor
+  FOREIGN KEY (ActorID)
+  REFERENCES Actor (ActorID)
+  PeliculaID integer,
+  CONSTRAINT fk_Pelicula
+  FOREIGN KEY (PeliculaID)
+  REFERENCES Pelicula (PeliculaID)
+);
+CREATE TABLE Pelicula (
+  PeliculaID integer PRIMARY KEY,
+  ProductoraID integer,
+  CONSTRAINT fk_Productora
+  FOREIGN KEY (ProductoraID)
+  REFERENCES Productora (ProductoraID),
+  NacionalidadID integer,
+    CONSTRAINT fk_Nacionalidad
+    FOREIGN KEY (NacionalidadID)
+    REFERENCES Nacionalidad (NacionalidadID),
+  Titulo varhar,
+  Fecha date
+);
+  
+  
+  
+
+
+
+
+
+
+
+
